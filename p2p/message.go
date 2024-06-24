b835289f0c44fe9863bd0b8c4bab47fce2ec71be
Turtel216 <papadimitrios216@gmail.com>
2024-06-24 17:55:29 +0300

    Refactored Message type

--- p2p/message.go
+++ p2p/message.go
@@ -2,9 +2,9 @@ package p2p
 
 import "net"
 
-// Message holds represents any arbitrary data that is being send over
+// RPC holds represents any arbitrary data that is being send over
 // each transport between two nodes in the networks
-type Message struct {
+type RPC struct {
 	From    net.Addr
 	Payload []byte
 }
